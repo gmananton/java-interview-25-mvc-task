c504d003d53c3611d56aea64637ed1309dae86dd
Михайлов Антон Владимирович <AVMikhaylov@alfabank.ru>
2025-12-16 02:01:35 +0300

    ...

---
+++ src/main/java/com/example/mvc/service/DemoService.java
@@ -0,0 +1,16 @@
+package com.example.mvc.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DemoService {
+
+    public String findById(String id) {
+        // TODO: бизнес-логика
+        return null;
+    }
+
+    public void create(String body) {
+        // TODO: бизнес-логика
+    }
+}
